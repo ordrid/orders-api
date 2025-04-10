9594bd0d9783c313be37fa13cbee5a7fb46df05f
Michael B. Espeña <espena.michael.pub@gmail.com>
2025-04-10 20:29:16 +0800

    feat: setup orders route handlers

--- application/routes.go
+++ application/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/ordrid/orders-api/handler"
 )
 
 func loadRoutes() *chi.Mux {
@@ -15,6 +16,17 @@ func loadRoutes() *chi.Mux {
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
+	router.Route("/orders", loadOrderRoutes)
 
 	return router
+}
+
+func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
 }
